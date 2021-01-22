bcfee0e37612b9cc24b11d186c0e3be44a5879ad
Amanda D <adnamadiaz@gmail.com>
2021-01-22 10:34:17 -0300

    Modify  class  styles for Card-sections and  NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import './NavBar.css';
+import logoMenu from '../assets/imgs/logo-03.png';
+import logoMenumobile from '../assets/imgs/logo-05.png';
 
 const NavBar = () => {
 
     return (
 
-        <nav >
-            <ul>
-                <li><a href="#acerca">Acerca de mí</a></li>
+        <nav className='box-navBar'>
+            <img
+                className="imgLogoMenuNavegacion not-visible-mobile"
+                src={logoMenu}
+                alt="Logo"
+            />
+            <img
+                className="imgLogoMenuNavegacion not-visible-desktop"
+                src={logoMenumobile}
+                alt="Logo"
+            />
+            <ul className="box-menu-desktop" >
+                <li><a href="https://houm.com/cl/propietario">Soy propietario</a></li>
+                <li><a href="https://me.houm.com/pagar">Pagar arriendo</a></li>
+                <li><a href="https://houm.com/cl/partner">Sé un  partner</a></li>
+                <li><a href="https://houm.com/cl/login">Mi cuenta</a></li>
             </ul>
         </nav>
 
